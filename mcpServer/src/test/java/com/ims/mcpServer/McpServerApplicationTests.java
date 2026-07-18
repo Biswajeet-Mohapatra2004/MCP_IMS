eefe43e36020868c674e4a6e154a164ee031d359
Biswajeet Mohapatra <mbiswajeet66@gmail.com>
2026-07-18 11:43:05 +0530

    initial project structure setup and dependency inclusion

---
+++ mcpServer/src/test/java/com/ims/mcpServer/McpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ims.mcpServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
