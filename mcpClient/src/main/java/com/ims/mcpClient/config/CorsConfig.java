3db99411a2139f952952f8bfcb967cb6d356c5e5
Biswajeet Mohapatra <mbiswajeet66@gmail.com>
2026-07-24 15:20:15 +0530

    Revert "minor fix#2"
    This reverts commit c9895fa530c248990ed77d063250fdfbaa6bb4ac.

--- mcpClient/src/main/java/com/ims/mcpClient/config/CorsConfig.java
+++ mcpClient/src/main/java/com/ims/mcpClient/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.allowed-origin}")
+    @Value("${APP.CORS.ALLOWED-ORIGIN}")
     private String allowedOrigin;
 
     @Override
