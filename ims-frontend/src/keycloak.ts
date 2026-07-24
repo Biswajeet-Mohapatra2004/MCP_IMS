1701505d938ee91d4358b4d7e81cc6338e3cc965
Biswajeet Mohapatra <mbiswajeet66@gmail.com>
2026-07-24 16:53:10 +0530

    keycloak issue#1

--- ims-frontend/src/keycloak.ts
+++ ims-frontend/src/keycloak.ts
@@ -4,6 +4,10 @@ const keycloak = new Keycloak({
    url: import.meta.env.VITE_KEYCLOAK_URL,
    realm: import.meta.env.VITE_KEYCLOAK_REALM,
    clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
+
+   console.log(import.meta.env.VITE_KEYCLOAK_URL);
+   console.log(import.meta.env.VITE_KEYCLOAK_REALM);
+   console.log(import.meta.env.VITE_KEYCLOAK_CLIENT_ID);
 });
 
 export default keycloak;
