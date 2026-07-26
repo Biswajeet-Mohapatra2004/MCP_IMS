3e3de0e1e40038535233394e12e5485bb97333f3
Biswajeet Mohapatra <mbiswajeet66@gmail.com>
2026-07-26 23:27:23 +0530

    frontend minor fix#4

--- ims-frontend/src/keycloak.ts
+++ ims-frontend/src/keycloak.ts
@@ -1,10 +1,6 @@
 import Keycloak from "keycloak-js";
 
-console.log("URL:", import.meta.env.VITE_KEYCLOAK_URL);
-console.log("Realm:", import.meta.env.VITE_KEYCLOAK_REALM);
-console.log("Client ID:", import.meta.env.VITE_KEYCLOAK_CLIENT_ID);
-
-const keycloak = new Keycloak({npm install recharts
+const keycloak = new Keycloak({
   url: import.meta.env.VITE_KEYCLOAK_URL,
   realm: import.meta.env.VITE_KEYCLOAK_REALM,
   clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
